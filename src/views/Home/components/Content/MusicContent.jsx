883aea513e6bf4efceabd0a918142b25457b6be6
bigboysuper6 <1773257425@qq.com>
2025-05-26 09:51:22 +0800

    Add error handling for API responses

--- src/views/Home/components/Content/MusicContent.jsx
+++ src/views/Home/components/Content/MusicContent.jsx
@@ -10,9 +10,11 @@ const MusicContent = (props) => {
     const [list, setList] = React.useState([]);
     const daySongsState = useSelector((state) => state.recommendSongs.value);
     const { status: usrState } = useSelector((state) => state.auth.value);
+
     React.useEffect(() => {
         getList();
     }, []);
+
     function getDayRcmCard() {
         if (usrState) {
             console.log("daySongsState1", daySongsState);
@@ -21,25 +23,51 @@ const MusicContent = (props) => {
             );
         } else return null;
     }
+
     async function getList() {
-        if (!usrState) {
-            const { data } = await personalized({ limit: 10 });
-            console.log("推荐的十个歌单", data);
-            setList(data.result);
-        } else {
-            const { data: usrData } = await recommendResource();
-            console.log("每日推荐歌单:", usrData.recommend);
-            let daysongs;
-            if (usrData.recommend.length > 8) {
-                daysongs = usrData.recommend.slice(0, 8);
-                console.log("裁剪为 8 首", daysongs);
-            } else if (usrData.recommend.length < 8) {
-                const { data } = await personalized(1);
-                console.log("获取一个推荐歌单", data.result);
-                daysongs = [...usrData.recommend, ...data.result];
-                console.log("新获取的八首推荐", daysongs);
-            } else daysongs = usrData.recommend;
-            setList(daysongs);
+        try {
+            if (!usrState) {
+                const response = await personalized({ limit: 10 });
+                if (response?.data?.result) {
+                    console.log("推荐的十个歌单", response.data);
+                    setList(response.data.result);
+                } else {
+                    console.error("Error fetching personalized data:", response);
+                    setList([]);
+                }
+            } else {
+                const response = await recommendResource();
+                const usrData = response?.data;
+                
+                if (!usrData?.recommend) {
+                    console.error("Error fetching recommended resources:", response);
+                    setList([]);
+                    return;
+                }
+
+                console.log("每日推荐歌单:", usrData.recommend);
+                let daysongs;
+                
+                if (usrData.recommend.length > 8) {
+                    daysongs = usrData.recommend.slice(0, 8);
+                    console.log("裁剪为 8 首", daysongs);
+                } else if (usrData.recommend.length < 8) {
+                    const personalizedResponse = await personalized(1);
+                    if (personalizedResponse?.data?.result) {
+                        console.log("获取一个推荐歌单", personalizedResponse.data.result);
+                        daysongs = [...usrData.recommend, ...personalizedResponse.data.result];
+                        console.log("新获取的八首推荐", daysongs);
+                    } else {
+                        daysongs = usrData.recommend;
+                    }
+                } else {
+                    daysongs = usrData.recommend;
+                }
+                setList(daysongs);
+            }
+        } catch (error) {
+            console.error("Error in getList:", error);
+            setList([]);
         }
     }
 
